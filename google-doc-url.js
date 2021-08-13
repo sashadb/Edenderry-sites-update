bfc829b82a39712a2d334b321e7c82df43005143
sashadb <sashadbrown@gmail.com>
2021-08-13 12:56:48 -0400

    delete google link

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1S_7ZejyraXTv1eFLFLeW9d4llc1t_cZU4GXgakv6uTs/edit#gid=0';
+var googleDocURL = '';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyD7f2Drkbza-ksOBy5Qq620efWBGCMdr7Y';
+var googleApiKey = '';
