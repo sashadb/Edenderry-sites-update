63c25cb26495b5f853cf20f966d01838352aaa40
sashadb <sashadbrown@gmail.com>
2021-08-13 13:08:59 -0400

    switch to csv

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,1 @@
-// paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = '';
 
-// insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = '';
